6712831bf0165902229806c9310b738a63a55acb
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-13 12:15:59 +0100

    done

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,13 +5,48 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Rappasoft\LaravelAuthenticationLog\Traits\AuthenticationLoggable;
+use Illuminate\Support\Str;
 
+/**
+ * @property string $id
+ * @property string $email
+ * @property string $name
+ * @property string $password
+ * @property string $accounttype
+ * @property string $role
+ */
 class Admin extends Authenticatable
 {
     use Notifiable;
     use HasFactory;
-    use AuthenticationLoggable;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 
     protected $guard = 'admin';
 
