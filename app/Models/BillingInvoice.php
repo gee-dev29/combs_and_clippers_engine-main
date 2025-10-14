b90f20fe37fac6aff1f5afc98c1277b61ae0c0ab
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-14 21:40:35 +0100

    8

--- app/Models/BillingInvoice.php
+++ app/Models/BillingInvoice.php
@@ -4,11 +4,40 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class BillingInvoice extends Model
 {
     use HasFactory;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
     protected $fillable = ['merchant_id', 'invoice_number', 'billing_date', 'next_billing_date', 'status', 'currency', 'amount', 'plan'];
 
     public $timestamps = false;
