9684514cad5a55d018ef9d6f3ac97ac3280fed01
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-15 16:38:37 +0100

    2

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -12,6 +12,34 @@ class BlogCategory extends Model
 {
     use HasFactory;
 
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
+
     protected $fillable= [
         'name',
         'slug',
