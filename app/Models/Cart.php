6d79a6caa219e800aa0bf544ae3c397858915acf
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-15 16:39:16 +0100

    6

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Str;
 
 class Cart extends Model
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 {
     protected $fillable = ['buyer_id', 'merchant_id', 'totalprice', 'status', 'items_count', 'currency', 'shipping', 'total_sum', 'max_delivery_period', 'min_delivery_period', 'max_delivery_date', 'min_delivery_date', 'delivery_type', 'coupon_id'];
 
