2b20ecfeda355fad4823afda4363bb95af968b03
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-15 16:38:55 +0100

    4

--- app/Models/BoothRentalPayment.php
+++ app/Models/BoothRentalPayment.php
@@ -7,11 +7,40 @@ use App\Models\UserStore;
 use App\Models\BoothRental;
 use App\Models\BoothRentPaymentHistory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class BoothRentalPayment extends Model
 {
     use HasFactory;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 
     protected $fillable = [
         'user_store_id',
