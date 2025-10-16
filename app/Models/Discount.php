5ef033a02e312dc5d02e691d5a73fcbaba778d85
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-16 12:07:25 +0100

    7

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Str;
 
 class Discount extends Model
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 {
     protected $fillable = [
         'merchant_id', 'discount_name', 'discount_type', 'discount', 'start_date', 'end_date'
