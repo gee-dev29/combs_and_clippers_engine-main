ad41b965170ae64542c9a93005376ceca5ceefd7
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-16 12:07:49 +0100

    done

--- app/Models/Dispute.php
+++ app/Models/Dispute.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Str;
 
 class Dispute extends Model
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 {
     const TYPE_REFUND = 'Refund';
     const TYPE_REPLACEMENT = 'Replace';
