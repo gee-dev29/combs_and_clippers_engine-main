8b2790a93f756a55866174b216d8c7aa8fcfb62d
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-17 13:48:28 +0100

    9

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,11 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
     use HasFactory;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
     use SoftDeletes;
     
     protected $fillable = ['thread_id', 'sender_id', 'receiver_id', 'message', 'attachment' ,'created_at', 'updated_at', 'deleted_at'];
