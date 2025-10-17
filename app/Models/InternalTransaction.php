1c754932203d65246adcff7856e5c919d307b7f7
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-17 13:47:51 +0100

    5

--- app/Models/InternalTransaction.php
+++ app/Models/InternalTransaction.php
@@ -4,10 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class InternalTransaction extends Model
 {
     use HasFactory;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
     protected $fillable = ['merchant_id', 'customer_id', 'order_id', 'type', 'transaction_ref', 'narration', 'currency', 'amount', 'payment_status'];
 
     public function customer()
