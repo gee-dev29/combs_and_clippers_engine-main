0161eef10c25e9b40f66c3752b61186bdc6d2f27
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-17 13:48:01 +0100

    6

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Str;
 
 class Invoice extends Model
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 {
     
     protected $fillable = ['merchant_id', 'customer_id', 'merchantAddress', 'merchantPhone', 'merchantEmail', 'merchantName', 'customerName', 'customerEmail', 'customerPhone', 'customerAddress', 'totalcost', 'vat', 'status', 'confirmed', 'items_count', 'deliveryPeriod', 'currency', 'invoiceRef', 'paymentRef', 'payment_status', 'payurl', 'invoice_type', 'startDate', 'endDate'];
