773d12d03e40a2f4c6ec8b10915f67fa98beb888
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-17 13:47:24 +0100

    2

--- app/Models/DisputeResolution.php
+++ app/Models/DisputeResolution.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Str;
 
 class DisputeResolution extends Model
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 { 
     protected $fillable = ['dispute_id', 'transcode', 'merchant_comment', 'customer_comment', 'arbitrator_comment', 'resolution_desc', 'sitting_date', 'next_sitting_date'];
     
