0b7b0f7dd5752c4dd88f72508243a26d45eab403
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-18 16:17:04 +0100

    1

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -3,9 +3,39 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Str;
 use App\Models\User;
 
 class Notification extends Model
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 {
     protected $fillable = ['user_id', 'title', 'description', 'view_link', 'viewed', 'type'];
     protected $table = 'user_notifications';
