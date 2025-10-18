80d74e636c21ee0cb20471a49cd05180b805e682
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-18 16:18:34 +0100

    5

--- app/Models/OrderLogistic.php
+++ app/Models/OrderLogistic.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Str;
 
 class OrderLogistic extends Model
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
 {
     protected $fillable = ['pickup_order_id', 'fulfilment_request_id', 'channel_grouping_id', 'channel_reference_id', 'order_id', 'cart_id', 'pickup_address_id', 'delivery_address_id', 'redis_key', 'rate_id', 'get_rates_key', 'kwik_key', 'delivery_note', 'type', 'estimated_days', 'currency', 'amount', 'delivery_status'];
     protected $table = 'order_logistics';
