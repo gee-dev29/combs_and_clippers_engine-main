943b7f5f7758a139e82420f2b8e43ff09b547b2b
Godwin Pro <maduegodwin2605@gmail.com>
2025-10-18 16:18:52 +0100

    6

--- app/Models/PackageBox.php
+++ app/Models/PackageBox.php
@@ -4,10 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class PackageBox extends Model
 {
     use HasFactory;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
     protected $fillable = ['box_size_id', 'name', 'description_image_url', 'height', 'width', 'length', 'max_weight'];
     protected $table = 'package_boxes';
 }
